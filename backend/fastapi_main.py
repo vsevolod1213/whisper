38f3cb80cd141029620d27b07d05667b8197eef4
vsevolod1213 <lvsevolod201@gmail.com>
2025-11-18 17:15:05 +0300

    zaebalo_eto_govno4

--- backend/fastapi_main.py
+++ backend/fastapi_main.py
@@ -23,7 +23,7 @@ app = FastAPI(title="Failety API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
