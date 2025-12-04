dcf597a76b363227b6aadc68c130fff208dfe740
vsevolod1213 <lvsevolod201@gmail.com>
2025-12-05 00:17:07 +0300

    correction get_daily_limit_for_user

--- backend/core/limits.py
+++ backend/core/limits.py
@@ -9,7 +9,7 @@ DAILY_LIMIT_PLUS_USER: int = 3600
 DAILY_LIMIT_PRO_USER: int = 6000
 DAILY_LIMIT_PREMIUM_USER: Optional[int] = None  
 
-def get_dayly_limit_for_user(user: "User") -> int | None:
+def get_daily_limit_for_user(user: "User") -> int | None:
     if user.tariff_plan == 0:
         return DAILY_LIMIT_FREE_USER
     elif user.tariff_plan == 1:
